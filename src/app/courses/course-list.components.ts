eb81d44038e8b3200b3ebb86dc97a9678d25f091
MacielMjose <jlmm92.maciel@gmail.com>
2020-06-20 17:56:00 -0300

    subida arquivos para repo remoto

---
+++ src/app/courses/course-list.components.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { courses } from './courses';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.components.html'
+})
+export class CourseListComponent implements OnInit{
+
+    courses:courses[] = [];
+      
+    ngOnInit():void{
+        this.courses = [
+            {
+                id:1,
+                name:'Angular: Forms',
+                imageurl:"",
+                price:99.99,
+                code:"xps-8796",
+                duration:120,
+                rating:5.4,
+                releaseDate:'December, 2, 2019'
+            }
+            ,
+            {
+                id:1,
+                name:'Angular: Http',
+                imageurl:"",
+                price:45.99,
+                code:"lkl-8796",
+                duration:80,
+                rating:4.5,
+                releaseDate:'December, 4, 2019'
+            }
+        ]
+    }
+}
